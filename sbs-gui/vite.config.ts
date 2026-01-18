a71e182b70a48498b048977a548cfde17e8b6261
wkusnierczyk <waclaw.kusnierczyk@gmail.com>
2026-01-18 11:11:23 +0100

    Enable https

--- sbs-gui/vite.config.ts
+++ sbs-gui/vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // This tells Vite: "If the browser asks for /solve, send it to the Rust backend"
     proxy: {
       '/solve': {
         target: 'http://localhost:8080',
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+      },
+    },
+  },
 })
