e7fafb7f9b2603ae26ea8577444dbde17124b54a
wkusnierczyk <waclaw.kusnierczyk@gmail.com>
2026-01-19 10:05:46 +0100

    Add release workflow (#18)
    * Add release workflow
    * Replace sed with perl

--- sbs-frontend/vite.config.ts
+++ sbs-frontend/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import packageJson from './package.json'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version),
+  },
   server: {
     proxy: {
       '/solve': {
